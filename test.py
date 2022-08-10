43c823aae7881c5da88dfcf41c97efed7ca18838
Girish Patel <patelg123@gmail.com>
2022-08-10 17:29:26 +0100

    added stemmer

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+from re import X
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
-
+from nltk.stem import PorterStemmer
+from porter2stemmer import Porter2Stemmer
 
 worf_quote = "Sir, I protest. I am not a merry man!"
 
@@ -17,5 +19,20 @@ filtered_list = [
 #for word in words_in_quote:
 #    if word.casefold() not in stop_words:
 #        filtered_list.append(word)
-        
-print(filtered_list)
+def printlist(list):
+    print(list)
+
+
+#printlist(filtered_list)
+
+stemmer = Porter2Stemmer()
+
+string_for_stemming = """
+The crew of the USS Discovery discovered many discoveries.
+Discovering is what explorers do."""
+
+words = word_tokenize(string_for_stemming)
+
+stemmed_words = [stemmer.stem(word) for word in words]
+
+printlist(stemmed_words)
